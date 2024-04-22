320b8eb66de0b0f365615f6a1bed56585ebc29a7
Konstantin Dobratulin <zsxoff@gmail.com>
2024-04-23 00:04:46 +0300

    Add Visual Studio Code installation

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@
 # Alacritty
 mkdir -p ${HOME}/.config/alacritty/
 ln -s ${PWD}/alacritty/alacritty.toml ${HOME}/.config/alacritty/alacritty.toml
+
+# Visual Studio Code
+if [[ $OSTYPE == 'darwin'* ]]; then
+    mkdir -p "${HOME}/Library/Application Support/Code/User/"
+    ln -s ${PWD}/Code/User/settings.json "${HOME}/Library/Application Support/Code/User/settings.json"
+fi
