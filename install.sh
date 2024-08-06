dd0f71527a87a1ed16f12a01da907b3317c27eac
Konstantin Dobratulin <zsxoff@gmail.com>
2024-08-06 22:35:48 +0300

    Add kitty install

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@
 mkdir -p ${HOME}/.config/alacritty/
 ln -s ${PWD}/alacritty/alacritty.toml ${HOME}/.config/alacritty/alacritty.toml
 
+# Kitty
+mkdir -p ${HOME}/.config/kitty/
+ln -s ${PWD}/kitty/kitty.conf ${HOME}/.config/kitty/kitty.conf
+ln -s ${PWD}/kitty/current-theme.conf ${HOME}/.config/kitty/current-theme.conf
+
 # Visual Studio Code
 if [[ $OSTYPE == 'darwin'* ]]; then
     mkdir -p "${HOME}/Library/Application Support/Code/User/"
